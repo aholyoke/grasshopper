4a3f394fe66b48dfa647e58245c552abfa608cfc
Alex Holyoke <aholyoke@uwaterloo.ca>
2016-04-17 10:33:37 -0700

    Add status codes. Add methods. Generalize routing table

--- fw.py
+++ fw.py
@@ -1,7 +1,29 @@
 CODES = {
     200: 'OK',
+    201: 'Created',
+    202: 'Accepted',
+    204: 'No Content',
+    301: 'Moved Permanently',
+    302: 'Found',
+    304: 'Not Modified',
+    307: 'Temporary Redirect',
+    308: 'Permanent Redirect',
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
     404: 'Not Found',
-    500: 'Internal Server Error'
+    405: 'Method Not Allowed',
+    408: 'Request Timeout',
+    409: 'Conflict',
+    410: 'Gone',
+    411: 'Length Required',
+    418: 'I\'m a teapot',  # super important
+    429: 'Too Many Requests',
+    500: 'Internal Server Error',
+    501: 'Not Implemented',
+    502: 'Bad Gateway',
+    503: 'Service Unavailable',
+    504: 'Gateway Timeout',
 }
 
 METHODS = [
@@ -9,21 +31,14 @@ METHODS = [
     'POST',
     'PUT',
     'DELETE',
+    'HEAD',
+    'PATCH',
 ]
 
 
 class Framework(object):
     def __init__(self):
-        self.routing_get = {}
-        self.routing_post = {}
-        self.routing_put = {}
-        self.routing_delete = {}
-        self.routing = {
-            'GET': self.routing_get,
-            'POST': self.routing_post,
-            'PUT': self.routing_put,
-            'DELETE': self.routing_delete,
-        }
+        self.routing = {verb: {} for verb in METHODS}
 
     def __call__(self, environ, start_response):
         method = environ['REQUEST_METHOD']
