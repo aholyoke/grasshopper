deaa0c419e0444d6312feb7994081bdc576a12b9
Alex Holyoke <aholyoke@uwaterloo.ca>
2016-04-28 12:33:11 -0400

    Make setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='grasshopper',
+      version='0.1',
+      description='The nothing-fancy WSGI framework',
+      url='http://github.com/aholyoke/grasshopper',
+      author='Alex Holyoke',
+      author_email='aholyoke@uwaterloo.ca',
+      license='BSD',
+      packages=['grasshopper'],
+      zip_safe=False)
