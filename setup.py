4b0fb6072648cc3e1c346ccf686d0c4b2db00094
Alex Holyoke <aholyoke@uwaterloo.ca>
2016-04-28 12:43:59 -0400

    Add package metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup
 setup(name='grasshopper',
       version='0.1',
       description='The nothing-fancy WSGI framework',
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+      ]
       url='http://github.com/aholyoke/grasshopper',
       author='Alex Holyoke',
       author_email='aholyoke@uwaterloo.ca',
