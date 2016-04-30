0e19c08b841a03043d6ce64cf8ab3c7512103c07
Alex Holyoke <aholyoke@uwaterloo.ca>
2016-04-30 11:29:21 -0400

    Fix syntax error

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='grasshopper',
         'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
-      ]
+      ],
       url='http://github.com/aholyoke/grasshopper',
       author='Alex Holyoke',
       author_email='aholyoke@uwaterloo.ca',
